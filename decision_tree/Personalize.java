dfa8aff9903ce2f57115e77bffd33d210eff68bb
tmontova <tmontova@umass.edu>
2014-04-29 00:47:32 -0400

    Just added some comments

--- decision_tree/Personalize.java
+++ decision_tree/Personalize.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-
+/**
+ * Responsible for personalizing ratings
+ * if the user has rated movies of the 
+ * same genre beforehand
+ *
+ */
 public class Personalize {
 	Hashtable<Integer, List<Data>> h = new Hashtable<Integer, List<Data>>();
 	
+	/**
+	 * Finds the average rating a user has given
+	 * movies of a similar genre before
+	 * adds the current rating to the list.
+	 */
 	public double rate(Data d, int prev){
 		List<Data> l;
 		int sum = 0;
@@ -33,8 +43,12 @@ public class Personalize {
 		return combine(sum, numRatings, prev);
 	}
 	
+	/**
+	 * combines the decision tree classification with the personalized classification
+	 */
 	private double combine(double personal, int numRate, double prev){
 		double tot = 0;
+		//increased weights the more movies the user has rated
 		for(int i = 0 ; i < numRate; i ++){
 			tot = (1-tot)/75 + tot;
 		}
